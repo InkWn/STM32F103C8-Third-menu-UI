aa76b96b56d50b0c2c301899481f96985cd3ccc3
InkWn <153103291+InkWn@users.noreply.github.com>
2026-05-16 23:48:31 +0800

    Update button.c

--- hardware/device/button/button.c
+++ hardware/device/button/button.c
@@ -5,14 +5,28 @@
 
 static GPIO_TypeDef *gpioA = GPIOA;
 
-
-void Get_Input(void) {
+void Button_Input_Check(void) {
 	for (uint8_t i = 0; i < KEY_COUNT; i++) {
-		if (!(gpioA->IDR & (0x01 << btnsGroup[i].pin))) {
-			btnsGroup[i].pushed = True;
+		__bool raw = (__bool)!(gpioA->IDR & (0x01 << btnsGroup[i].pin));
+		if ((raw != btnsGroup[i].lastRaw)) {
+			// 改变了状态
+			btnsGroup[i].lastRaw = raw;
+			btnsGroup[i].pending = True;
+		}
+	}
+}
+
+void Button_Input_Confirm(void) {
+	for (uint8_t i = 0; i < KEY_COUNT; i++) {
+		if (!btnsGroup[i].pending) continue;
+
+		__bool raw = (__bool)!(gpioA->IDR & (0x01 << btnsGroup[i].pin));
+		if (raw == btnsGroup[i].lastRaw) {
+			btnsGroup[i].pushed = raw;
+			if (!raw) btnsGroup[i].handled = False;
+			btnsGroup[i].pending = False;
 		} else {
-			btnsGroup[i].pushed = False;
-			btnsGroup[i].handled = False;
+			btnsGroup[i].lastRaw = raw;
 		}
 	}
 }
